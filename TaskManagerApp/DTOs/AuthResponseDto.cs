efce085a39cf6bf4c449968de177218c9c12e9b4
Fidan Tomarova <fidan.tomarova@gmail.com>
2025-07-17 14:06:02 +0400

    Comment added

--- TaskManagerApp/DTOs/AuthResponseDto.cs
+++ TaskManagerApp/DTOs/AuthResponseDto.cs
@@ -2,6 +2,7 @@
 {
     public class AuthResponseDto
     {
+        //authentication response DTO
         public string Token { get; set; } = null!;
         public string UserName { get; set; } = null!;
         public string Role { get; set; } = null!;
