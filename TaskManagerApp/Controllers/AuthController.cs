701923e7a2e106b4624f4965595b95401d23d4d6
Fidan Tomarova <fidan.tomarova@gmail.com>
2025-07-18 15:00:01 +0400

    Add AuthController and setup JWT settings

---
+++ TaskManagerApp/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerApp.DTOs;
+using TaskManagerApp.Services;
+
+namespace TaskManagerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        #region Register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            if (registerRequestDto == null)
+            {
+                return BadRequest("Invalid registration request.");
+            }
+            var response = await _authService.RegisterAsync(registerRequestDto);
+            if (!response.IsSuccess)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+        #endregion
+
+        #region Login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            if (loginRequestDto == null)
+            {
+                return BadRequest("Invalid login request.");
+            }
+            var response = await _authService.LoginAsync(loginRequestDto);
+            if (!response.IsSuccess)
+            {
+                return Unauthorized(response);
+            }
+            return Ok(response);
+        }
+        #endregion
+    }
+}
--- TaskManagerApp/Program.cs
+++ TaskManagerApp/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using TaskManagerApp.DAL;
 using TaskManagerApp.Repositories;
 using TaskManagerApp.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddControllers();
 
@@ -17,6 +22,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
+
+// JWT Authentication Configuration
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -33,6 +54,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
